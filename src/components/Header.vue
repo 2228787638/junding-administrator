bec01fae6ef9b090fdb5e4ff3c4937ef2eb78cba
2228787638 <2228787638@qq.com>
2019-09-19 23:34:38 +0800

    引入elementUI

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">用户列表</template>
+          <el-menu-item index="2-1">新增用户</el-menu-item>
+          <el-menu-item index="2-2">查看用户</el-menu-item>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">房源列表</template>
+          <el-menu-item index="3-1">新增房源</el-menu-item>
+          <el-menu-item index="3-2">查看房源</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="4">收入</el-menu-item>
+      </el-menu>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Header></Header>
+    <div>{{msg}}</div>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+export default {
+  name: '',
+  components: {Header},
+  data () {
+    return {
+      msg: 'Hello Vue.js'
+    }
+  },
+  component: {
+  }
+}
+</script>
+<style scoped>
+
+</style>
