3a1a01dbef8ce43efc0d6afed7f43e483fd9a19e
SaigyoujiKonpaku <1414242145@qq.com>
2019-10-07 23:51:46 +0800

    添加请求拦截

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
 import Router from '../router'
+import store from '../store'
 
 axios.defaults.baseURL = 'http://ygfygf.cn'
-axios.defaults.headers.common['token'] = localStorage.getItem('token')
+// axios.defaults.headers.common['token'] = localStorage.getItem('token')
 axios.defaults.timeout = 5000
 
 axios.interceptors.response.use(
@@ -16,6 +17,25 @@ axios.interceptors.response.use(
   },
   error => {
     console.log(error)
+  }
+)
+axios.interceptors.request.use(
+  config => {
+    // do something before request is sent
+
+    if (store.state.common.token) {
+      // let each request carry token
+      // ['X-Token'] is a custom headers key
+      // please modify it according to the actual situation
+      console.log('没token')
+      config.headers['token'] = localStorage.getItem('token')
+    }
+    return config
+  },
+  error => {
+    // do something with request error
+    console.log(error) // for debug
+    return Promise.reject(error)
   }
 )
 
